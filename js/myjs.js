a78ca5cc1bfbaf55bac87e30079070d53516390e
chenming <17864154681@163.com>
2020-03-10 18:11:46 +0800

    try

---
+++ js/myjs.js
@@ -0,0 +1,9 @@
+$('#right>img').hover(function(){
+	$(this).css('transform','scale(1.2,1.2)');
+	$(this).css('marginBottom','30px');
+	$(this).css('marginTop','20px');
+	},function(){
+	$(this).css('transform','scale(1,1)');
+	$(this).css('marginBottom','10px');
+	$(this).css('marginTop','0px');
+	});
