068c51dcdafc299a46b3c7ff82b54feab295b4c2
chenming <17864154681@163.com>
2020-05-14 01:19:56 +0800

    use Loading Component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import styles from './index.css';
+
+class Loading extends Component {
+    
+    constructor(props) {
+        super(props);
+    }
+    
+
+    componentDidMount() {
+    }
+
+    componentWillUnmount() {
+    }
+
+    render() {
+        return 'loading...'
+    }
+
+}
+
+export default Loading;
--- src/utils/loadingComponent.js
+++ src/utils/loadingComponent.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Loading from '../components/Loading';
 export default function loadingComponentWrapper(Component, judgeFn) {
     return function loadingComponent(props) {
         if(judgeFn.call(null, props)) {
-            return 'loading...';
+            return <Loading></Loading>;
         }
         else {
             return <Component {...props}></Component>
