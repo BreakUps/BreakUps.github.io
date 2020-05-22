ffae99eca8462381892a4ab5bbcda2e0007384fb
chenming <17864154681@163.com>
2020-05-22 22:18:39 +0800

    fix category sort order

--- scripts/generate.js
+++ scripts/generate.js
@@ -17,7 +17,7 @@ fs.readdir(DIRPATH, function (err, files) {
             date: mtime
         };
     });
-    category.sort((a, b) => a < b ? -1 : 1);
+    category.sort((a, b) => a.date > b.date ? -1 : 1);
     const fileDateMap = JSON.parse(fs.readFileSync(CATEGORYPATH, 'utf-8')).reduce((pre, cur) => {
         const { title, date } = cur;
         pre.set(title, Date.parse(date));
