de2378e5ce75774158e7b8000f1be65a8eb8943c
chenming <17864154681@163.com>
2020-06-03 00:23:04 +0800

    add highlight for code

--- src/pages/post/Article/index.js
+++ src/pages/post/Article/index.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
 import marked from 'marked';
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import  'highlight.js/styles/ocean.css';
 import styles from './index.css';
 import loadingComponentWrapper from '../../../utils/loadingComponent';
+
+hljs.registerLanguage('javascript', javascript);
+const options = {
+    highlight: (code, lang, callback) => {
+        if(!lang) {
+            callback(null, code);
+        }
+        else {
+            callback(null, hljs.highlight(lang, code).value);
+        }
+    }
+}
+
+marked.setOptions(options);
 
 class Article extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            article: ''
+        };
+    }
+
+    componentDidMount() {
+        marked(this.props.content, (err, html) => {
+            if(err) {
+                return;
+            }
+            this.setState({
+                article: html
+            });
+        });
     }
 
     render() {
-        const { content } = this.props;
-        return <div dangerouslySetInnerHTML={{__html: marked(content)}} className={styles.article}></div>
+        const { article } = this.state;
+        return <div dangerouslySetInnerHTML={{__html: article}} className={styles.article}></div>
     }
 
 }
