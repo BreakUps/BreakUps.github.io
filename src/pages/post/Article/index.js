a380b8db3244b33cffff9d2c3464d55698fd418c
chenming <17864154681@163.com>
2024-07-04 03:50:25 +0800

    chore: use markedHighlight to add langPrefix

--- src/pages/post/Article/index.js
+++ src/pages/post/Article/index.js
@@ -1,33 +1,28 @@
 import React, { Component } from 'react';
-import marked from 'marked';
+import { Marked } from 'marked';
+import { markedHighlight } from 'marked-highlight';
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 import typescript from 'highlight.js/lib/languages/typescript';
-import  'highlight.js/styles/ocean.css';
+import  'highlight.js/styles/github.css';
 import styles from './index.css';
 import loadingComponentWrapper from '../../../utils/loadingComponent';
 
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('typescript', typescript);
-const options = {
-    highlight: (code, lang, callback) => {
-        if(!lang) {
-            callback(null, code);
-        }
-        else {
-            callback(null, hljs.highlight(lang, code).value);
-        }
-    }
-}
-
-marked.setOptions(options);
+const marked = new Marked(
+    markedHighlight({
+      langPrefix: 'hljs language-',
+      highlight(code, lang) {
+        const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+        return hljs.highlight(code, { language }).value;
+      }
+    })
+  );
 
 class Article extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            article: ''
-        };
     }
 
     toTop(e) {
@@ -39,22 +34,9 @@ class Article extends Component {
     }
 
 
-
-    componentDidMount() {
-        marked(this.props.content, (err, html) => {
-            if(err) {
-                return;
-            }
-            this.setState({
-                article: html
-            });
-        });
-    }
-
     render() {
-        const { article } = this.state;
         return  <>
-                    <div dangerouslySetInnerHTML={{__html: article}} className={styles.article}></div>
+                    <div dangerouslySetInnerHTML={{__html: marked.parse(this.props.content)}} className={styles.article}></div>
                     <div className={styles['top-button-box']}>
                         <a href="#top" className={styles['top-button']} onClick={this.toTop} title="回顶部">&gt;</a>
                     </div>
